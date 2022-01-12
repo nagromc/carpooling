6d1cdb8c175464645f3473a0d7888d134d6e67a4
Morgan Courbet <github@sub.ploki.fr>
2022-01-12 13:39:12 +0100

    feat(domain): implement `ListCarpoolersUseCase`

--- carpooling-domain/src/main/java/org/carpooling/domain/Carpooler.java
+++ carpooling-domain/src/main/java/org/carpooling/domain/Carpooler.java
@@ -1,6 +1,10 @@
 package org.carpooling.domain;
 
-public record Carpooler(String id) {
+public record Carpooler(String id, String displayName) {
+
+  public Carpooler(String id) {
+    this(id, "Anonymous");
+  }
 
   @Override
   public String toString() {
